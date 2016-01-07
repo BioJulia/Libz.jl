afbbeef242e531a1223fc400af584f5aa15317bd
Sam O'Connor <sam@octech.com.au>
2016-01-07 18:22:19 +1100

    Convenience functions for dealing with .gz files
    Convenience functions to read/write directly from/to a named file.
    
    I'm doing a cleanup of my local stash of convenience functions and thought that these might be generally useful.
    
    ```julia
    gzwrite("/tmp/foo.gz", "hello")
    gzreadall("/tmp/foo.gz")
    "hello"
    ```

--- src/Libz.jl
+++ src/Libz.jl
@@ -6,6 +6,7 @@ using BufferedStreams
 
 export ZlibInflateInputStream, ZlibDeflateInputStream,
        ZlibInflateOutputStream, ZlibDeflateOutputStream,
+       gzopen, gzwrite, gzreadbytes, gzreadall,
        adler32, crc32
 
 
@@ -28,6 +29,24 @@ end
 
 const decompress = inflate
 
+
+function gzopen(f::Function, filename::AbstractString, mode)
+    @assert mode == "w"
+    open(filename, mode) do io
+        zio = ZlibDeflateOutputStream(io)
+        try f(zio)
+        finally close(zio)
+        end
+    end
+end
+
+function gzopen(f::Function, filename::AbstractString)
+    open(io->f(ZlibInflateInputStream(io)), filename)
+end
+
+gzwrite(filename::AbstractString, data) = gzopen(io->write(io, data), filename, "w")
+gzreadall(filename::AbstractString) = gzopen(readall, filename)
+gzreadbytes(filename::AbstractString) = gzopen(readbytes, filename)
 
 end # module Libz
 
